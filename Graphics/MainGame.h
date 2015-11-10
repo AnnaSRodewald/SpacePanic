57a445de0310d6ab0120315c8718d8a94d0b9e83
Anna Sophie Rodewald <anna.sophie.rodewald@sap.com>
2015-11-10 19:11:48 +0100

    First commit

---
+++ Graphics/MainGame.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Windows.h>
+
+#include <SDL/SDL.h>
+#include <GL/glew.h>
+
+
+enum class GameState{PLAY, EXIT};
+
+
+class MainGame
+	{
+	public:
+		MainGame(void);
+		~MainGame(void);
+
+		void run();
+
+		
+
+
+
+	private:
+		void initSystems();
+
+		void gameLoop();
+		void processInput();
+
+		SDL_Window* _window;
+		int _screenWidth;
+		int _screenHeight;
+
+		GameState _gameState;
+	};
+
---
+++ Graphics/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MainGame.h"
+
+
+int main(int argc, char** argv) {
+	MainGame mainGame;
+	mainGame.run();
+
+	return 0;
+	//SDL_Init(SDL_INIT_EVERYTHING);
+
+	//	return 0;
+	}
