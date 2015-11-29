e068562af0b50220727f8ae12be4a98f6c4e4029
Anna Sophie Rodewald <anna.sophie.rodewald@sap.com>
2015-11-29 23:59:50 +0100

    Completed Advanced C++/Graphics Tutorial 18: Custom Library, Bengine

---
+++ GameEngine/GLSLProgram.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <GL/glew.h>
+
+class GLSLProgram
+	{
+	public:
+		GLSLProgram(void);
+		~GLSLProgram(void);
+
+		void compileShaders(const std::string& vertexShaderFilePath, const std::string& fragementShaderFilePath);
+
+		void linkShaders();
+
+		void addAttribute(const std::string& attributeName);
+
+		GLint getUniformLocation(const std::string& uniformName);
+
+		void use();
+
+		void unuse();
+
+
+	private:
+		int _numAttributes;
+
+		void compileShaders(const std::string& filePath, GLuint shaderID);
+
+		GLuint _programID;
+
+		GLuint _vertexShaderID;
+		GLuint _fragmentShaderID;
+
+		
+
+
+	};
+
